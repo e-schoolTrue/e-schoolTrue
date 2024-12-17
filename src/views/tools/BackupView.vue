d0a52d3a2e0453da46a13fb1d2190810df0d48b5
briand444 <fabry0550@gmail.com>
2024-12-17 18:05:27 +0100

    conflit

---
+++ src/views/tools/BackupView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useBackupStore } from '@/stores/backupStore';
+import { storeToRefs } from 'pinia';
+import { onMounted } from 'vue';
+
+const backupStore = useBackupStore();
+const { history, stats, isBackupInProgress } = storeToRefs(backupStore);
+
+onMounted(async () => {
+    try {
+        await backupStore.initializeBackupSystem();
+    } catch (error) {
+        console.error('Erreur lors de l\'initialisation:', error);
+    }
+});
+</script> 
