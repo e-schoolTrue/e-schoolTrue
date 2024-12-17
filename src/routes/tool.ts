f3ccea721c0b6990a845a15ba32bcec6ca213881
briand <evelinbrid@gmal.com>
2024-12-17 17:20:55 +0100

    mises en place route

--- src/routes/tool.ts
+++ src/routes/tool.ts
@@ -8,5 +8,16 @@ export const toolRoutes = [
       path: "/tools/generate-id",
       name: "StudentCard",
       component: () => import("@/views/tools/StudentCardView.vue"),
-    }
+    },
+    {
+      path: "/tools/school-report",
+      name: "ReportCard",
+      component: () => import("@/views/tools/ReportCardView.vue"),
+    },
+    {
+      path: "/tools/save-data",
+      name: "Backup",
+      component: () => import("@/views/tools/BackupView.vue"),
+    },
+    
   ]; 
